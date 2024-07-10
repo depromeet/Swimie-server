bb210d0105d895fa9c92e45ee96d7029c76250e0
penrose15 <seongju99@naver.com>
2024-07-10 19:33:44 +0900

    feat: kakao oauth2 인증 추가 (#16)
    CustomOAuth2UserService, CustomOAuth2User : registrationId 식별 후 유저 정보 가져옴

---
+++ module-presentation/src/main/java/com/depromeet/security/oauth/CustomOAuth2UserService.java
@@ -0,0 +1,49 @@
+package com.depromeet.security.oauth;
+
+import com.depromeet.member.Member;
+import com.depromeet.member.mapper.MemberMapper;
+import com.depromeet.member.repository.MemberRepository;
+import com.depromeet.security.oauth.dto.KakaoResponse;
+import com.depromeet.security.oauth.dto.MemberDto;
+import com.depromeet.security.oauth.dto.OAuth2Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String registrationId = userRequest.getClientRegistration().getRegistrationId();
+
+        OAuth2Response oAuth2Response;
+        if (registrationId.equals("kakao")) {
+            oAuth2Response = new KakaoResponse(oAuth2User.getAttributes());
+        } else {
+            throw new OAuth2AuthenticationException(
+                    "failed to resolve registration id " + registrationId);
+        }
+
+        String nickname = oAuth2Response.getName();
+        String email = oAuth2Response.getEmail();
+
+        Member member = findByEmailOrSave(email, nickname);
+        return new CustomOAuth2User(new MemberDto(email, member.getRole()));
+    }
+
+    private Member findByEmailOrSave(String email, String nickname) {
+        return memberRepository
+                .findByEmail(email)
+                .orElse(memberRepository.save(MemberMapper.from(nickname, email)));
+    }
+}
