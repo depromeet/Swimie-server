4a549dbb9e614a06686055e4d0d004e9cf0c8f4f
its-sky <smc9919@naver.com>
2024-07-13 16:19:59 +0900

    remove: 불필요한 로거 삭제 (#33)

--- module-presentation/src/main/java/com/depromeet/pool/api/PoolController.java
+++ module-presentation/src/main/java/com/depromeet/pool/api/PoolController.java
@@ -5,13 +5,11 @@ import com.depromeet.pool.dto.response.PoolResponseDto;
 import com.depromeet.pool.service.PoolService;
 import com.depromeet.type.pool.PoolSuccessType;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/pool")
