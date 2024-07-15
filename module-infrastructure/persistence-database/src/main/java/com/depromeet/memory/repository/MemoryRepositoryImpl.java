6007c7a0cbc2beffd071d7690695694c9867b27d
ywonchae1 <ywonchae62@gmail.com>
2024-07-15 17:47:15 +0900

    feat: MemoryRepository findById 구현 (#40)

--- module-infrastructure/persistence-database/src/main/java/com/depromeet/memory/repository/MemoryRepositoryImpl.java
+++ module-infrastructure/persistence-database/src/main/java/com/depromeet/memory/repository/MemoryRepositoryImpl.java
@@ -5,6 +5,8 @@ import com.depromeet.memory.entity.MemoryEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 @RequiredArgsConstructor
 public class MemoryRepositoryImpl implements MemoryRepository {
@@ -13,5 +15,10 @@ public class MemoryRepositoryImpl implements MemoryRepository {
     @Override
     public Memory save(Memory memory) {
         return memoryJpaRepository.save(MemoryEntity.from(memory)).toModel();
+    }
+
+    @Override
+    public Optional<Memory> findById(Long memoryId) {
+        return memoryJpaRepository.findById(memoryId).map(MemoryEntity::toModel);
     }
 }
