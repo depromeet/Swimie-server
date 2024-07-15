f812e195cee0294455af3bad0f05b5ac67dd9279
ywonchae1 <ywonchae62@gmail.com>
2024-07-15 10:41:12 +0900

    refactor: 수영 기록 저장 응답 형식 간소화 (#32)

--- module-presentation/src/main/java/com/depromeet/memory/controller/MemoryController.java
+++ module-presentation/src/main/java/com/depromeet/memory/controller/MemoryController.java
@@ -7,27 +7,21 @@ import com.depromeet.type.memory.MemorySuccessType;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
-@RequestMapping("/api/memory")
 @RequiredArgsConstructor
+@RequestMapping("/api/memory")
 public class MemoryController {
     private final MemoryService memoryService;
 
     @PostMapping
     @Operation(summary = "수영 기록 저장")
-    public ResponseEntity<ApiResponse<?>> create(
-            @Valid @RequestBody MemoryCreateRequest memoryCreateRequest) {
+    public ApiResponse<?> create(@Valid @RequestBody MemoryCreateRequest memoryCreateRequest) {
         memoryService.save(memoryCreateRequest);
-        return ResponseEntity.status(HttpStatus.CREATED)
-                .body(ApiResponse.success(MemorySuccessType.POST_RESULT_SUCCESS));
+        return ApiResponse.success(MemorySuccessType.POST_RESULT_SUCCESS);
     }
 }
