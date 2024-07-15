ec4a9813114127a465a0c4db80e2bb3cd1186cd2
penrose15 <seongju99@naver.com>
2024-07-15 22:01:19 +0900

    fix: controller response successType 변경 및 어노테이션 위치 변경 (#42)

--- module-presentation/src/main/java/com/depromeet/image/api/ImageController.java
+++ module-presentation/src/main/java/com/depromeet/image/api/ImageController.java
@@ -1,7 +1,6 @@
 package com.depromeet.image.api;
 
-import static com.depromeet.type.image.ImageSuccessType.DELETE_IMAGES_SUCCESS;
-import static com.depromeet.type.image.ImageSuccessType.UPLOAD_IMAGES_SUCCESS;
+import static com.depromeet.type.image.ImageSuccessType.*;
 
 import com.depromeet.dto.response.ApiResponse;
 import com.depromeet.image.dto.request.ImagesMemoryIdDto;
@@ -27,44 +26,44 @@ public class ImageController {
     private final ImageGetService imageGetService;
     private final ImageDeleteService imageDeleteService;
 
-    @Operation(summary = "수영 기록 이미지 s3에 업로드")
     @PostMapping
+    @Operation(summary = "수영 기록 이미지 s3에 업로드")
     public ApiResponse<?> uploadImages(@RequestPart List<MultipartFile> images) {
         List<Long> imageIds = imageUploadService.uploadMemoryImages(images);
 
         return ApiResponse.success(UPLOAD_IMAGES_SUCCESS, imageIds);
     }
 
-    @Operation(summary = "업로드 된 이미지에 memoryId 추가")
     @PatchMapping("/memory") // 임시로 작성하긴 했는데 직관적이지 않네요 api 작명 추천 받습니다.
+    @Operation(summary = "업로드 된 이미지에 memoryId 추가")
     public ApiResponse<?> addMemoryToImages(
             @RequestParam(name = "memoryId") Long memoryId,
             @RequestBody ImagesMemoryIdDto imagesMemoryIdDto) {
         imageUploadService.addMemoryIdToImages(memoryId, imagesMemoryIdDto);
 
-        return ApiResponse.success(UPLOAD_IMAGES_SUCCESS);
+        return ApiResponse.success(ADD_MEMORY_TO_IMAGES_SUCCESS);
     }
 
-    @Operation(summary = "수영 기록의 이미지 수정")
     @PatchMapping
+    @Operation(summary = "수영 기록의 이미지 수정")
     public ApiResponse<?> updateImages(
             @RequestParam(name = "memoryId") Long memoryId,
             @RequestPart List<MultipartFile> images) {
         imageUpdateService.updateImages(memoryId, images);
 
-        return ApiResponse.success(UPLOAD_IMAGES_SUCCESS);
+        return ApiResponse.success(UPDATE_IMAGES_SUCCESS);
     }
 
-    @Operation(summary = "수영 기록의 이미지 조회")
     @GetMapping
+    @Operation(summary = "수영 기록의 이미지 조회")
     public ApiResponse<?> findImages(@RequestParam(name = "memoryId") Long memoryId) {
         List<MemoryImagesDto> memoryImages = imageGetService.findImagesByMemoryId(memoryId);
 
-        return ApiResponse.success(UPLOAD_IMAGES_SUCCESS, memoryImages);
+        return ApiResponse.success(GET_IMAGES_SUCCESS, memoryImages);
     }
 
-    @Operation(summary = "Delete images belongs to memory", description = "수영 기록의 이미지 삭제")
     @DeleteMapping
+    @Operation(summary = "Delete images belongs to memory", description = "수영 기록의 이미지 삭제")
     public ApiResponse<?> deleteImages(@RequestParam(value = "memoryId") Long memoryId) {
         imageDeleteService.deleteAllImagesByMemoryId(memoryId);
 
