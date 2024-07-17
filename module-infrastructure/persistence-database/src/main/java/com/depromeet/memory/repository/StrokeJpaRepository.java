fc6772b997ebf0475b6eec280bdaec8de2cc9ec4
its-sky <smc9919@naver.com>
2024-07-17 17:00:15 +0900

    style: Spotless 적용 (#54)

--- module-infrastructure/persistence-database/src/main/java/com/depromeet/memory/repository/StrokeJpaRepository.java
+++ module-infrastructure/persistence-database/src/main/java/com/depromeet/memory/repository/StrokeJpaRepository.java
@@ -3,5 +3,4 @@ package com.depromeet.memory.repository;
 import com.depromeet.memory.entity.StrokeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StrokeJpaRepository extends JpaRepository<StrokeEntity, Long> {
-}
+public interface StrokeJpaRepository extends JpaRepository<StrokeEntity, Long> {}
