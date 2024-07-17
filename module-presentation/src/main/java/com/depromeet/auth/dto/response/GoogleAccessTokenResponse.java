631e92b87ead5022fe7354e6e9cb28b2f7405c5a
its-sky <smc9919@naver.com>
2024-07-17 23:27:39 +0900

    fix: 코드 버그 픽스 및 Spotless 적용 (#58)

--- module-presentation/src/main/java/com/depromeet/auth/dto/response/GoogleAccessTokenResponse.java
+++ module-presentation/src/main/java/com/depromeet/auth/dto/response/GoogleAccessTokenResponse.java
@@ -3,15 +3,8 @@ package com.depromeet.auth.dto.response;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record GoogleAccessTokenResponse(
-        @JsonProperty("access_token")
-        String accessToken,
-        @JsonProperty("expires_in")
-        Integer expiresIn,
-        @JsonProperty("scope")
-        String scope,
-        @JsonProperty("token_type")
-        String tokenType,
-        @JsonProperty("id_token")
-        String idToken
-) {
-}
+        @JsonProperty("access_token") String accessToken,
+        @JsonProperty("expires_in") Integer expiresIn,
+        @JsonProperty("scope") String scope,
+        @JsonProperty("token_type") String tokenType,
+        @JsonProperty("id_token") String idToken) {}
