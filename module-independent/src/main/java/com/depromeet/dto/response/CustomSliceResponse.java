25e8bee1492f1a6142efd708a623dd6548e860f7
penrose15 <seongju99@naver.com>
2024-07-18 22:38:11 +0900

    refactor: record로 변경 (#50)

--- module-independent/src/main/java/com/depromeet/dto/response/CustomSliceResponse.java
+++ module-independent/src/main/java/com/depromeet/dto/response/CustomSliceResponse.java
@@ -1,9 +1,7 @@
 package com.depromeet.dto.response;
 
 import lombok.Builder;
-import lombok.Getter;
 
-@Getter
 public record CustomSliceResponse<T>(
         T content, int pageNumber, int pageSize, boolean hasNext, boolean hasPrevious) {
 
