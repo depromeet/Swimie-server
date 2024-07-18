a5c01108a051b4301350c122ce9d49c84dd10f4d
hsj <seongju99@naver.com>
2024-07-18 12:06:27 +0900

    feat: TimelineController 작성 (#50)

--- module-presentation/src/main/java/com/depromeet/memory/controller/MemoryApi.java
+++ module-presentation/src/main/java/com/depromeet/memory/controller/MemoryApi.java
@@ -2,13 +2,21 @@ package com.depromeet.memory.controller;
 
 import com.depromeet.dto.response.ApiResponse;
 import com.depromeet.memory.dto.request.MemoryCreateRequest;
+import com.depromeet.security.LoginMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "수영 기록(Memory)")
 public interface MemoryApi {
     @Operation(summary = "수영 기록 저장")
     ApiResponse<?> create(@Valid @RequestBody MemoryCreateRequest memoryCreateRequest);
+
+    @Operation(summary = "최신 기록부터 내림차순 타임라인 조회")
+    ApiResponse<?> getTimeline(
+            @RequestParam(name = "cursorId", required = false) Long cursorId,
+            @RequestParam(name = "size") int size,
+            @LoginMember Long memberId);
 }
