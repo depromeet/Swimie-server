fdb101b46502ff549dcfde17cbeb66a5dd67587d
its-sky <smc9919@naver.com>
2024-07-19 19:14:28 +0900

    refactor: Memory 파사드 패턴 적용 (#73)

--- module-presentation/src/main/java/com/depromeet/memory/service/MemoryService.java
+++ module-presentation/src/main/java/com/depromeet/memory/service/MemoryService.java
@@ -1,11 +1,12 @@
 package com.depromeet.memory.service;
 
+import com.depromeet.member.Member;
 import com.depromeet.memory.Memory;
 import com.depromeet.memory.dto.request.MemoryCreateRequest;
 import com.depromeet.memory.dto.response.MemoryResponse;
 
 public interface MemoryService {
-    Memory save(MemoryCreateRequest memoryCreateRequest);
+    Memory save(Member member, MemoryCreateRequest request);
 
     MemoryResponse findById(Long memoryId);
 }
--- module-presentation/src/main/java/com/depromeet/memory/service/MemoryServiceImpl.java
+++ module-presentation/src/main/java/com/depromeet/memory/service/MemoryServiceImpl.java
@@ -1,10 +1,7 @@
 package com.depromeet.memory.service;
 
-import com.depromeet.exception.InternalServerException;
 import com.depromeet.exception.NotFoundException;
-import com.depromeet.exception.UnauthorizedException;
 import com.depromeet.member.Member;
-import com.depromeet.member.repository.MemberRepository;
 import com.depromeet.memory.Memory;
 import com.depromeet.memory.MemoryDetail;
 import com.depromeet.memory.dto.request.MemoryCreateRequest;
@@ -13,10 +10,7 @@ import com.depromeet.memory.repository.MemoryDetailRepository;
 import com.depromeet.memory.repository.MemoryRepository;
 import com.depromeet.pool.Pool;
 import com.depromeet.pool.repository.PoolRepository;
-import com.depromeet.security.AuthorizationUtil;
-import com.depromeet.type.member.MemberErrorType;
 import com.depromeet.type.memory.MemoryErrorType;
-import com.depromeet.type.pool.PoolErrorType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,46 +18,28 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @RequiredArgsConstructor
 public class MemoryServiceImpl implements MemoryService {
+    private final PoolRepository poolRepository;
     private final MemoryRepository memoryRepository;
     private final MemoryDetailRepository memoryDetailRepository;
 
-    private final MemberRepository memberRepository;
-    private final AuthorizationUtil authorizationUtil;
-
-    private final PoolRepository poolRepository;
-
     @Transactional
-    public Memory save(MemoryCreateRequest memoryCreateRequest) {
-        Long loginId = authorizationUtil.getLoginId();
-        Member writer =
-                memberRepository
-                        .findById(loginId)
-                        .orElseThrow(() -> new UnauthorizedException(MemberErrorType.NOT_FOUND));
-        MemoryDetail memoryDetail = getMemoryDetail(memoryCreateRequest);
+    public Memory save(Member writer, MemoryCreateRequest request) {
+        MemoryDetail memoryDetail = getMemoryDetail(request);
         if (memoryDetail != null) {
             memoryDetail = memoryDetailRepository.save(memoryDetail);
         }
-        Pool pool = null;
-        if (memoryCreateRequest.getPoolId() != null) {
-            pool =
-                    poolRepository
-                            .findById(memoryCreateRequest.getPoolId())
-                            .orElseThrow(() -> new NotFoundException(PoolErrorType.NOT_FOUND));
-        }
+        Pool pool = poolRepository.findById(request.getPoolId()).orElse(null);
         Memory memory =
                 Memory.builder()
                         .member(writer)
                         .pool(pool)
                         .memoryDetail(memoryDetail)
-                        .recordAt(memoryCreateRequest.getRecordAt())
-                        .startTime(memoryCreateRequest.getStartTime())
-                        .endTime(memoryCreateRequest.getEndTime())
-                        .lane(memoryCreateRequest.getLane())
-                        .diary(memoryCreateRequest.getDiary())
+                        .recordAt(request.getRecordAt())
+                        .startTime(request.getStartTime())
+                        .endTime(request.getEndTime())
+                        .lane(request.getLane())
+                        .diary(request.getDiary())
                         .build();
-        if (memory == null) {
-            throw new InternalServerException(MemoryErrorType.CREATE_FAILED);
-        }
         return memoryRepository.save(memory);
     }
 
