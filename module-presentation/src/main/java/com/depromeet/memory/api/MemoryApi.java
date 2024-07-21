d7627d31df86e63351971293d0b1f8307c9970c4
penrose15 <seongju99@naver.com>
2024-07-21 23:21:52 +0900

    fix: timeline-calendar api의 파라미터 설명 추가

--- module-presentation/src/main/java/com/depromeet/memory/api/MemoryApi.java
+++ module-presentation/src/main/java/com/depromeet/memory/api/MemoryApi.java
@@ -6,6 +6,7 @@ import com.depromeet.memory.dto.request.MemoryUpdateRequest;
 import com.depromeet.memory.dto.response.MemoryResponse;
 import com.depromeet.security.LoginMember;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,9 +31,19 @@ public interface MemoryApi {
     @Operation(summary = "타임라인 최신순 조회 및 달력 조회 후, 위/아래 무한 스크롤 구현")
     ApiResponse<?> timelineCalendar(
             @LoginMember Long memberId,
-            @RequestParam(value = "cursorId", required = false) Long cursorId,
-            @RequestParam(value = "cursorRecordAt", required = false) String cursorRecordAt,
-            @RequestParam(value = "date", required = false) String date,
-            @RequestParam(value = "showNewer", required = false) boolean showNewer,
-            @RequestParam(value = "size") Integer size);
+            @Parameter(description = "최초 조회 이후 나온 timeline 리스트 중 가장 마지막 요소의 memory PK")
+                    @RequestParam(value = "cursorId", required = false)
+                    Long cursorId,
+            @Parameter(description = "최초 조회 이후 나온 timeline 리스트 중 가장 마지막 요소의 memory recordAt")
+                    @RequestParam(value = "cursorRecordAt", required = false)
+                    String cursorRecordAt,
+            @Parameter(description = "조회하고 싶은 날짜, yyyy-MM-dd", example = "2024-07-15")
+                    @RequestParam(value = "date", required = false)
+                    String date,
+            @Parameter(
+                            description =
+                                    "조회하고 싶은 날짜 조회 이후, 날짜 기준 이전 정보를 보고 싶다면 false, 이후 정보를 보고 싶다면 true")
+                    @RequestParam(value = "showNewer", required = false)
+                    boolean showNewer,
+            @Parameter(description = "페이지 크기") @RequestParam(value = "size") Integer size);
 }
