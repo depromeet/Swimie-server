a1f088f69a0e7ed9309b3bfa5b2456c568bb7d50
ywonchae1 <ywonchae62@gmail.com>
2024-07-23 01:58:51 +0900

    mod: member 단일 조회 응답 형식 수정 (#81)

--- module-presentation/src/main/java/com/depromeet/member/api/MemberController.java
+++ module-presentation/src/main/java/com/depromeet/member/api/MemberController.java
@@ -1,7 +1,9 @@
 package com.depromeet.member.api;
 
+import com.depromeet.dto.response.ApiResponse;
 import com.depromeet.member.dto.response.MemberFindOneResponseDto;
 import com.depromeet.member.service.MemberService;
+import com.depromeet.type.member.MemberSuccessType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +17,7 @@ public class MemberController implements MemberApi {
     private final MemberService memberService;
 
     @GetMapping("/{id}")
-    public MemberFindOneResponseDto getMember(@PathVariable Long id) {
-        return memberService.findOneMemberResponseById(id);
+    public ApiResponse<MemberFindOneResponseDto> getMember(@PathVariable("id") Long id) {
+        return ApiResponse.success(MemberSuccessType.GET_SUCCESS, memberService.findOneMemberResponseById(id));
     }
 }
