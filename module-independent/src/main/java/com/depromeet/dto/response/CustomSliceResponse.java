040a132a8d4794880361fb2a5a55175c001ee4ea
penrose15 <seongju99@naver.com>
2024-07-22 10:48:32 +0900

    fix: CustomSliceResponse 컬럼 변경

--- module-independent/src/main/java/com/depromeet/dto/response/CustomSliceResponse.java
+++ module-independent/src/main/java/com/depromeet/dto/response/CustomSliceResponse.java
@@ -2,8 +2,7 @@ package com.depromeet.dto.response;
 
 import lombok.Builder;
 
-public record CustomSliceResponse<T>(
-        T content, int pageNumber, int pageSize, boolean hasNext, boolean hasPrevious) {
+public record CustomSliceResponse<T>(T content, int pageNumber, int pageSize, boolean hasNext) {
 
     @Builder
     public CustomSliceResponse {}
