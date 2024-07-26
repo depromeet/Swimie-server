c37f6a9aeab424fa8eb4c9cb808129d0ad263933
its-sky <smc9919@naver.com>
2024-07-26 18:07:40 +0900

    chore: 검색 결과의 null 값 payload에 미포함 (#108)

--- module-presentation/src/main/java/com/depromeet/pool/dto/response/PoolSearchResponse.java
+++ module-presentation/src/main/java/com/depromeet/pool/dto/response/PoolSearchResponse.java
@@ -1,25 +1,11 @@
 package com.depromeet.pool.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record PoolSearchResponse(
         List<PoolInfoResponse> poolInfos, int pageSize, Long cursorId, boolean hasNext) {
-    // public static PoolSearchResponse of(List<Pool> pools, Set<Long> favoritePoolIds) {
-    //     if (pools == null) {
-    //         return new PoolSearchResponse(null);
-    //     }
-    //     return new PoolSearchResponse(
-    //             pools.stream()
-    //                     .map(
-    //                             pool ->
-    //                                     new PoolInfoResponse(
-    //                                             pool.getId(),
-    //                                             pool.getName(),
-    //                                             pool.getAddress(),
-    //                                             favoritePoolIds.contains(pool.getId())))
-    //                     .toList());
-    // }
-
     public static PoolSearchResponse of(
             List<PoolInfoResponse> poolInfos, Long cursorId, boolean hasNext) {
         return new PoolSearchResponse(poolInfos, poolInfos.size(), cursorId, hasNext);
