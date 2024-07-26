72585130837e9d83f9dd82fb59e159233451bce9
penrose15 <seongju99@naver.com>
2024-07-26 13:21:57 +0900

    feat: repository 메서드 이름 변경 및 이미지 조회시 memoryId + ImageUploadStatus 로 기준 변경 (#91)

--- module-infrastructure/persistence-database/src/main/java/com/depromeet/image/repository/ImageRepositoryImpl.java
+++ module-infrastructure/persistence-database/src/main/java/com/depromeet/image/repository/ImageRepositoryImpl.java
@@ -2,6 +2,9 @@ package com.depromeet.image.repository;
 
 import com.depromeet.image.Image;
 import com.depromeet.image.entity.ImageEntity;
+import com.depromeet.image.entity.QImageEntity;
+import com.depromeet.memory.ImageUploadStatus;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +14,9 @@ import org.springframework.stereotype.Repository;
 @RequiredArgsConstructor
 public class ImageRepositoryImpl implements ImageRepository {
     private final ImageJpaRepository imageJpaRepository;
+    private final JPAQueryFactory queryFactory;
+
+    QImageEntity image = QImageEntity.imageEntity;
 
     @Override
     public Long save(Image image) {
@@ -28,15 +34,36 @@ public class ImageRepositoryImpl implements ImageRepository {
     }
 
     @Override
+    public void updateByImageIds(List<Long> imageIds) {
+        queryFactory
+                .update(image)
+                .set(image.imageUploadStatus, ImageUploadStatus.UPLOADED)
+                .where(image.id.in(imageIds));
+    }
+
+    @Override
     public Optional<Image> findById(Long id) {
         return imageJpaRepository.findById(id).map(ImageEntity::toModel);
     }
 
     @Override
-    public List<Image> findImagesByMemoryId(Long memoryId) {
+    public List<Image> findAllByMemoryId(Long memoryId) {
         return imageJpaRepository.findByMemoryId(memoryId).stream()
                 .map(ImageEntity::toModel)
                 .toList();
+    }
+
+    @Override
+    public List<Image> findAllByMemoryIdAndHasUploaded(Long memoryId) {
+        List<ImageEntity> images =
+                queryFactory
+                        .selectFrom(image)
+                        .where(
+                                image.memory.id.eq(memoryId),
+                                image.imageUploadStatus.eq(ImageUploadStatus.UPLOADED))
+                        .fetch();
+
+        return images.stream().map(ImageEntity::toModel).toList();
     }
 
     @Override
