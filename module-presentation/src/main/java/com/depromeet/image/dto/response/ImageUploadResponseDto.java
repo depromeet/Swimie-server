54fe499e332a9086454904ef9f0d18afddcec258
penrose15 <seongju99@naver.com>
2024-07-27 00:27:33 +0900

    refactor: facade 헥사고날 아키텍처에 맞게 수정 (#107)

--- module-presentation/src/main/java/com/depromeet/image/dto/response/ImageUploadResponseDto.java
+++ module-presentation/src/main/java/com/depromeet/image/dto/response/ImageUploadResponseDto.java
@@ -1,5 +1,6 @@
 package com.depromeet.image.dto.response;
 
+import com.depromeet.image.port.out.command.ImagePresignedUrlCommand;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
@@ -7,4 +8,12 @@ import lombok.Builder;
 public record ImageUploadResponseDto(Long imageId, String imageName, String presignedUrl) {
     @Builder
     public ImageUploadResponseDto {}
+
+    public static ImageUploadResponseDto of(ImagePresignedUrlCommand imagePresignedUrlCommand) {
+        return ImageUploadResponseDto.builder()
+                .imageId(imagePresignedUrlCommand.imageId())
+                .imageName(imagePresignedUrlCommand.imageName())
+                .presignedUrl(imagePresignedUrlCommand.presignedUrl())
+                .build();
+    }
 }
