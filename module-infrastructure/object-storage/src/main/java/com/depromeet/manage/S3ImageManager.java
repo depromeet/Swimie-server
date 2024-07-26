6745f59f89165749bfd7c868957995f44d21c200
penrose15 <seongju99@naver.com>
2024-07-27 00:24:47 +0900

    refactor: s3관련 기능 module-infrastructure로 이동 (#107)

---
+++ module-infrastructure/object-storage/src/main/java/com/depromeet/manage/S3ImageManager.java
@@ -0,0 +1,48 @@
+package com.depromeet.manage;
+
+import com.depromeet.image.port.out.s3.S3ManagePort;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+
+@Component
+@RequiredArgsConstructor
+public class S3ImageManager implements S3ManagePort {
+    private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
+
+    @Override
+    public String getPresignedUrl(String imageName) {
+        PutObjectRequest putObjectRequest =
+                PutObjectRequest.builder().bucket(bucketName).key(imageName).build();
+
+        PutObjectPresignRequest putObjectPresignRequest =
+                PutObjectPresignRequest.builder()
+                        .signatureDuration(Duration.ofMinutes(5))
+                        .putObjectRequest(putObjectRequest)
+                        .build();
+
+        PresignedPutObjectRequest presignedPutObjectRequest =
+                s3Presigner.presignPutObject(putObjectPresignRequest);
+
+        return presignedPutObjectRequest.url().toString();
+    }
+
+    @Override
+    public void deleteImageFromS3(String imageName) {
+        DeleteObjectRequest deleteObjectRequest =
+                DeleteObjectRequest.builder().bucket(bucketName).key(imageName).build();
+
+        s3Client.deleteObject(deleteObjectRequest);
+    }
+}
