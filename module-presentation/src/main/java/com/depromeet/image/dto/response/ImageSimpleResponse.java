7e82d6dd21d6e2098d1cb890c4906ace318d68a3
ywonchae1 <ywonchae62@gmail.com>
2024-07-31 17:11:40 +0900

    mod: 기록 상세 부분 이미지 응답 형태 수정 (#94)

--- module-presentation/src/main/java/com/depromeet/image/dto/response/ImageSimpleResponse.java
+++ module-presentation/src/main/java/com/depromeet/image/dto/response/ImageSimpleResponse.java
@@ -3,13 +3,12 @@ package com.depromeet.image.dto.response;
 import com.depromeet.image.domain.Image;
 import lombok.Builder;
 
-public record ImageSimpleResponse(Long imageId, String imageName, String url) {
+public record ImageSimpleResponse(String imageName, String url) {
     @Builder
     public ImageSimpleResponse {}
 
     public static ImageSimpleResponse of(Image image) {
         return ImageSimpleResponse.builder()
-                .imageId(image.getId())
                 .imageName(image.getImageName())
                 .url(image.getImageUrl())
                 .build();
