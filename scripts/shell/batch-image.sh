66465d86de26478a688358330c2f4dd77806c673
its-sky <smc9919@naver.com>
2024-08-01 20:06:28 +0900

    docs: 배치 실행 쉘 스크립트 내용 수정 (#122)

--- scripts/shell/batch-image.sh
+++ scripts/shell/batch-image.sh
@@ -6,12 +6,10 @@ container_name=image-batch
 request_date=$(date +"%Y%m%d")
 
 echo "----------------------------------------------------------------------"
-# docker image pull
 echo "> 도커 이미지 pull 받기"
 docker pull ${user_id}/${server_name}
 
-# green_port로 서버 실행
 echo "> 9090 포트로 PENDING 이미지 일괄 삭제 배치 작업 컨테이너 실행"
-echo "> docker run -d --name ${container_name} -p 9090:9090 -e TZ=Asia/Seoul ${user_id}/${server_name}"
+echo "> docker run -d --name ${container_name} -p 9090:9090 -e requestDate=${request_date} -e TZ=Asia/Seoul ${user_id}/${server_name}"
 docker run -d --name ${container_name} -p 9090:9090 -e requestDate=${request_date} -e TZ=Asia/Seoul ${user_id}/${server_name}
 echo "----------------------------------------------------------------------"
