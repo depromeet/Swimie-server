12a02aeb7216320668b53b8749680e444729e925
ywonchae1 <ywonchae62@gmail.com>
2024-08-01 09:13:43 +0900

    mod: 기록 응답 swagger 형태 수정 (#130)

--- module-presentation/src/main/java/com/depromeet/memory/dto/response/MemoryResponse.java
+++ module-presentation/src/main/java/com/depromeet/memory/dto/response/MemoryResponse.java
@@ -11,6 +11,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,9 +27,13 @@ public class MemoryResponse {
     private MemoryDetailResponse memoryDetail;
     private List<StrokeResponse> strokes;
     private List<ImageSimpleResponse> images;
+    @Schema(description = "작성일자", example = "2024-08-01", maxLength = 10, type = "string")
     private LocalDate recordAt;
+    @Schema(description = "시작시간", example = "11:00", maxLength = 8, type = "string")
     private LocalTime startTime;
+    @Schema(description = "종료시간", example = "11:50", maxLength = 8, type = "string")
     private LocalTime endTime;
+    @Schema(description = "운동시간", example = "00:50", maxLength = 8, type = "string")
     private LocalTime duration;
     private Short lane;
     private Float totalLap;
