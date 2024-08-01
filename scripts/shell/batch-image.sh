693b33f7280374555522731a05baf0d83f514042
its-sky <smc9919@naver.com>
2024-08-01 19:44:05 +0900

    fix: 배치 실행 쉘 스크립트 버그 수정 (#122)

--- scripts/shell/batch-image.sh
+++ scripts/shell/batch-image.sh
@@ -13,5 +13,5 @@ docker pull ${user_id}/${server_name}
 # green_port로 서버 실행
 echo "> 9090 포트로 PENDING 이미지 일괄 삭제 배치 작업 컨테이너 실행"
 echo "> docker run -d --name ${container_name} -p 9090:9090 -e TZ=Asia/Seoul ${user_id}/${server_name}"
-docker run -d --name ${container_name} -p 9090:9090 -e requestDate=${request_date} TZ=Asia/Seoul ${user_id}/${server_name}
+docker run -d --name ${container_name} -p 9090:9090 -e requestDate=${request_date} -e TZ=Asia/Seoul ${user_id}/${server_name}
 echo "----------------------------------------------------------------------"
