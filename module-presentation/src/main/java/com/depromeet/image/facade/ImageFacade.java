b70c56ec2c2dafdd3b2672f9b4ed03467d77d629
penrose15 <seongju99@naver.com>
2024-08-03 22:39:18 +0900

    fix: QA에서 발견한 오류 수정

--- module-presentation/src/main/java/com/depromeet/image/facade/ImageFacade.java
+++ module-presentation/src/main/java/com/depromeet/image/facade/ImageFacade.java
@@ -1,7 +1,9 @@
 package com.depromeet.image.facade;
 
+import com.depromeet.exception.BadRequestException;
 import com.depromeet.image.domain.Image;
 import com.depromeet.image.domain.vo.ImagePresignedUrlVo;
+import com.depromeet.image.dto.request.ImageNameRequest;
 import com.depromeet.image.dto.response.ImageResponse;
 import com.depromeet.image.dto.response.ImageUploadResponse;
 import com.depromeet.image.port.in.ImageDeleteUseCase;
@@ -10,6 +12,8 @@ import com.depromeet.image.port.in.ImageUpdateUseCase;
 import com.depromeet.image.port.in.ImageUploadUseCase;
 import com.depromeet.memory.domain.Memory;
 import com.depromeet.memory.service.MemoryService;
+import com.depromeet.type.image.ImageErrorType;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,13 +29,22 @@ public class ImageFacade {
     private final ImageUpdateUseCase imageUpdateUseCase;
     private final ImageDeleteUseCase imageDeleteUseCase;
 
-    public List<ImageUploadResponse> getPresignedUrlAndSaveImages(List<String> imageNames) {
+    public List<ImageUploadResponse> getPresignedUrlAndSaveImages(
+            ImageNameRequest imageNameRequest) {
+        if (imageNameRequest.imageNames() == null) {
+            throw new BadRequestException(ImageErrorType.IMAGES_CANNOT_BE_EMPTY);
+        }
         List<ImagePresignedUrlVo> imagePresignedUrlVos =
-                imageUploadUseCase.getPresignedUrlAndSaveImages(imageNames);
+                imageUploadUseCase.getPresignedUrlAndSaveImages(imageNameRequest.imageNames());
         return imagePresignedUrlVos.stream().map(ImageUploadResponse::of).toList();
     }
 
-    public List<ImageUploadResponse> updateImages(Long memoryId, List<String> imageNames) {
+    public List<ImageUploadResponse> updateImages(
+            Long memoryId, ImageNameRequest imageNameRequest) {
+        List<String> imageNames = new ArrayList<>();
+        if (imageNameRequest.imageNames() != null) {
+            imageNames = imageNameRequest.imageNames();
+        }
         Memory memory = memoryService.findById(memoryId);
         List<ImagePresignedUrlVo> imagePresignedUrlVos =
                 imageUpdateUseCase.updateImages(memory, imageNames);
