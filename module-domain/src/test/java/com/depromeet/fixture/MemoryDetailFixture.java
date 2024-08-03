482661549ef59526e87d6e557e8be157021a93ea
penrose15 <seongju99@naver.com>
2024-08-03 14:45:58 +0900

    test: TimelineServiceTest 수정 (#150)

--- module-domain/src/test/java/com/depromeet/fixture/MemoryDetailFixture.java
+++ module-domain/src/test/java/com/depromeet/fixture/MemoryDetailFixture.java
@@ -4,9 +4,8 @@ import com.depromeet.memory.domain.MemoryDetail;
 import java.time.LocalTime;
 
 public class MemoryDetailFixture {
-    public static MemoryDetail make(Long id) {
+    public static MemoryDetail make() {
         return MemoryDetail.builder()
-                .id(id)
                 .item("킥판")
                 .heartRate((short) 100)
                 .pace(LocalTime.of(0, 30))
