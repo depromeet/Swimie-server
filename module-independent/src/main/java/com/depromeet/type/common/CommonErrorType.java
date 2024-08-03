98da75503a8eb72a3bd8e2338568000023ee8876
its-sky <smc9919@naver.com>
2024-08-04 03:18:39 +0900

    refactor: 예외 핸들링 세부 사항 추가 (#163)

--- module-independent/src/main/java/com/depromeet/type/common/CommonErrorType.java
+++ module-independent/src/main/java/com/depromeet/type/common/CommonErrorType.java
@@ -11,7 +11,9 @@ public enum CommonErrorType implements ErrorType {
     INTERNAL_SERVER("COMMON_6", "알 수 없는 서버 에러가 발생했습니다"),
     VALIDATION_FAILED("COMMON_7", "정상적이지 않은 입력값입니다"),
     BAD_REQUEST("COMMON_8", "잘못된 요청입니다"),
-    MISSING_PARAM("COMMON_9", "요청 파라미터가 누락되었습니다");
+    MISSING_PARAM("COMMON_9", "요청 파라미터가 누락되었습니다"),
+    RUNTIME("COMMON_10", "런타임 예외가 발생했습니다"),
+    IO("COMMON_11", "I/O 예외가 발생했습니다");
 
     private final String code;
     private final String message;
