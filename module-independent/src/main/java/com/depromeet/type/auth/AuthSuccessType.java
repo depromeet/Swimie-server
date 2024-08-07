4b97d86c033f393d5cacf5df288f97803681da0f
ywonchae1 <ywonchae62@gmail.com>
2024-08-07 15:40:03 +0900

    feat: error, success type 추가 (#181)

--- module-independent/src/main/java/com/depromeet/type/auth/AuthSuccessType.java
+++ module-independent/src/main/java/com/depromeet/type/auth/AuthSuccessType.java
@@ -5,7 +5,8 @@ import com.depromeet.type.SuccessType;
 public enum AuthSuccessType implements SuccessType {
     LOGIN_SUCCESS("AUTH_1", "로그인에 성공하였습니다"),
     REISSUE_ACCESS_TOKEN_SUCCESS("AUTH_2", "JWT ACCESS 토큰 발급에 성공하였습니다"),
-    LOGOUT_SUCCESS("AUTH_3", "로그아웃에 성공하였습니다");
+    LOGOUT_SUCCESS("AUTH_3", "로그아웃에 성공하였습니다"),
+    DELETE_ACCOUNT_SUCCESS("AUTH_4", "회원 탈퇴에 성공하였습니다");
 
     private final String code;
 
