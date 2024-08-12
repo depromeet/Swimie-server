e8533648084b0cd13c2aee2cfe790ce46faf8da2
penrose15 <seongju99@naver.com>
2024-08-12 21:49:35 +0900

    feat: FollowService 코드 작성 (#200)

--- module-domain/src/main/java/com/depromeet/friend/port/in/FollowUseCase.java
+++ module-domain/src/main/java/com/depromeet/friend/port/in/FollowUseCase.java
@@ -1,7 +1,18 @@
 package com.depromeet.friend.port.in;
 
+import com.depromeet.friend.domain.vo.FollowSlice;
+import com.depromeet.friend.domain.vo.Follower;
+import com.depromeet.friend.domain.vo.Following;
 import com.depromeet.member.domain.Member;
 
 public interface FollowUseCase {
-    void addOrDeleteFollowing(Member member, Member following);
+    boolean addOrDeleteFollowing(Member member, Member following);
+
+    FollowSlice<Following> getFollowingByMemberIdAndCursorId(Long memberId, Long cursorId);
+
+    FollowSlice<Follower> getFollowerByMemberIdAndCursorId(Long memberId, Long cursorId);
+
+    int countFollowingByMemberId(Long memberId);
+
+    int countFollowerByMemberId(Long memberId);
 }
--- module-domain/src/main/java/com/depromeet/friend/service/FollowService.java
+++ module-domain/src/main/java/com/depromeet/friend/service/FollowService.java
@@ -1,6 +1,9 @@
 package com.depromeet.friend.service;
 
 import com.depromeet.friend.domain.Friend;
+import com.depromeet.friend.domain.vo.FollowSlice;
+import com.depromeet.friend.domain.vo.Follower;
+import com.depromeet.friend.domain.vo.Following;
 import com.depromeet.friend.port.in.FollowUseCase;
 import com.depromeet.friend.port.out.persistence.FriendPersistencePort;
 import com.depromeet.member.domain.Member;
@@ -16,16 +19,37 @@ public class FollowService implements FollowUseCase {
     private final FriendPersistencePort friendPersistencePort;
 
     @Transactional
-    public void addOrDeleteFollowing(Member member, Member following) {
+    public boolean addOrDeleteFollowing(Member member, Member following) {
         Optional<Friend> existedFollowing =
                 friendPersistencePort.findByMemberIdAndFollowingId(
                         member.getId(), following.getId());
         if (existedFollowing.isPresent()) {
             friendPersistencePort.deleteByMemberIdAndFollowingId(member.getId(), following.getId());
-            return;
+            return false;
         }
 
         Friend friend = Friend.builder().member(member).following(following).build();
         friendPersistencePort.addFollowing(friend);
+        return true;
+    }
+
+    @Override
+    public FollowSlice<Following> getFollowingByMemberIdAndCursorId(Long memberId, Long cursorId) {
+        return friendPersistencePort.findFollowingsByMemberIdAndCursorId(memberId, cursorId);
+    }
+
+    @Override
+    public FollowSlice<Follower> getFollowerByMemberIdAndCursorId(Long memberId, Long cursorId) {
+        return friendPersistencePort.findFollowersByMemberIdAndCursorId(memberId, cursorId);
+    }
+
+    @Override
+    public int countFollowingByMemberId(Long memberId) {
+        return friendPersistencePort.countFollowingByMemberId(memberId);
+    }
+
+    @Override
+    public int countFollowerByMemberId(Long memberId) {
+        return friendPersistencePort.countFollowerByMemberId(memberId);
     }
 }
