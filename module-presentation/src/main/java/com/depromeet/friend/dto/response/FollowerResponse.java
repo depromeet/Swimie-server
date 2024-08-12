57d7d89e708444fea9397097c209ea9dd0ee7f35
penrose15 <seongju99@naver.com>
2024-08-12 21:51:01 +0900

    feat: DTO 수정 (#200)

--- module-presentation/src/main/java/com/depromeet/friend/dto/response/FollowerResponse.java
+++ module-presentation/src/main/java/com/depromeet/friend/dto/response/FollowerResponse.java
@@ -3,7 +3,12 @@ package com.depromeet.friend.dto.response;
 import lombok.Builder;
 
 public record FollowerResponse(
-        Long memberId, String name, String profile, String message, boolean hasFollowedBack) {
+        Long friendId,
+        Long memberId,
+        String name,
+        String profile,
+        String message,
+        boolean hasFollowedBack) {
     @Builder
     public FollowerResponse {}
 }
--- module-presentation/src/main/java/com/depromeet/friend/dto/response/FollowingResponse.java
+++ module-presentation/src/main/java/com/depromeet/friend/dto/response/FollowingResponse.java
@@ -1,8 +1,16 @@
 package com.depromeet.friend.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.NonNull;
 
-public record FollowingResponse(Long followingId, String name, String profile, String message) {
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record FollowingResponse(
+        @NonNull Long friendId,
+        @NonNull Long memberId,
+        @NonNull String name,
+        String profile,
+        String message) {
     @Builder
     public FollowingResponse {}
 }
