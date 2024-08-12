52c10fe30822aea498e6c6a1398f6c7d2f025156
its-sky <smc9919@naver.com>
2024-08-12 20:52:44 +0900

    feat: Reaction 성공/실패 타입 구현 (#203)

---
+++ module-independent/src/main/java/com/depromeet/type/reaction/ReactionErrorType.java
@@ -0,0 +1,26 @@
+package com.depromeet.type.reaction;
+
+import com.depromeet.type.ErrorType;
+
+public enum ReactionErrorType implements ErrorType {
+    SAME_USER("REACTION_1", "자신의 기록에 응원을 등록할 수 없습니다"),
+    MAXIMUM_FAILED("REACTION_2", "하나의 기록에 최대 3개까지 응원을 등록할 수 있습니다");
+
+    private final String code;
+    private final String message;
+
+    ReactionErrorType(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
---
+++ module-independent/src/main/java/com/depromeet/type/reaction/ReactionSuccessType.java
@@ -0,0 +1,25 @@
+package com.depromeet.type.reaction;
+
+import com.depromeet.type.SuccessType;
+
+public enum ReactionSuccessType implements SuccessType {
+    POST_REACTION_SUCCESS("REACTION_1", "응원 등록에 성공하였습니다");
+
+    private final String code;
+    private final String message;
+
+    ReactionSuccessType(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return this.code;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
