b1f68d6a5e6d33f7871983f3461e1f0a4eeeeb5d
ywonchae1 <ywonchae62@gmail.com>
2024-08-14 11:04:06 +0900

    fix: apple redirect url 수정 (#208)

--- module-presentation/src/main/java/com/depromeet/auth/facade/AuthFacade.java
+++ module-presentation/src/main/java/com/depromeet/auth/facade/AuthFacade.java
@@ -64,7 +64,7 @@ public class AuthFacade {
     public JwtTokenResponse loginByApple(AppleLoginRequest request) {
         final AccountProfileResponse profile =
                 socialUseCase.getAppleAccountToken(
-                        request.code(), "https://unduly-light-chimp.ngrok-free.app");
+                        request.code(), "https://swimie.life");
         if (profile == null) {
             throw new NotFoundException(AuthErrorType.NOT_FOUND);
         }
