e28344f0c22df7b99aed12ab7815e19301b61b4b
its-sky <smc9919@naver.com>
2024-08-15 02:34:44 +0900

    fix: 응원 생성 API Request Body Validation 수정 (#203)

--- module-presentation/src/main/java/com/depromeet/reaction/dto/request/ReactionCreateRequest.java
+++ module-presentation/src/main/java/com/depromeet/reaction/dto/request/ReactionCreateRequest.java
@@ -1,14 +1,17 @@
 package com.depromeet.reaction.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 public record ReactionCreateRequest(
         @Schema(description = "기록 ID", example = "1", requiredMode = Schema.RequiredMode.REQUIRED)
                 @NotNull
                 Long memoryId,
         @Schema(description = "이모지", example = "🦭", requiredMode = Schema.RequiredMode.REQUIRED)
-                @NotNull
+                @NotBlank
+                @Size(max = 2)
                 String emoji,
         @Schema(
                         description = "코멘트",
