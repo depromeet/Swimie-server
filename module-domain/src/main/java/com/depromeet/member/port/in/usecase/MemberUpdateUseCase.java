ae797df1f623a8be60ff770a0762bd155878ff01
ywonchae1 <ywonchae62@gmail.com>
2024-08-14 23:59:03 +0900

    fix: update 메서드 인터페이스 이동 (#213)

--- module-domain/src/main/java/com/depromeet/member/port/in/usecase/MemberUpdateUseCase.java
+++ module-domain/src/main/java/com/depromeet/member/port/in/usecase/MemberUpdateUseCase.java
@@ -2,9 +2,15 @@ package com.depromeet.member.port.in.usecase;
 
 import com.depromeet.member.domain.Member;
 import com.depromeet.member.domain.MemberGender;
+import com.depromeet.member.port.in.command.UpdateMemberCommand;
 
 public interface MemberUpdateUseCase {
+
+    Member update(UpdateMemberCommand command);
+
     Member updateNickname(Long memberId, String nickname);
 
     Member updateGender(Long memberId, MemberGender gender);
+
+    Member updateProfileImageUrl(Long memberId, String profileImageUrl);
 }
--- module-domain/src/main/java/com/depromeet/member/port/in/usecase/MemberUseCase.java
+++ module-domain/src/main/java/com/depromeet/member/port/in/usecase/MemberUseCase.java
@@ -3,7 +3,6 @@ package com.depromeet.member.port.in.usecase;
 import com.depromeet.member.domain.Member;
 import com.depromeet.member.domain.vo.MemberSearchPage;
 import com.depromeet.member.port.in.command.SocialMemberCommand;
-import com.depromeet.member.port.in.command.UpdateMemberCommand;
 
 public interface MemberUseCase {
     Member findById(Long id);
@@ -13,6 +12,4 @@ public interface MemberUseCase {
     void deleteById(Long id);
 
     MemberSearchPage searchMemberByName(String nameQuery, Long cursorId);
-
-    Member update(UpdateMemberCommand command);
 }
