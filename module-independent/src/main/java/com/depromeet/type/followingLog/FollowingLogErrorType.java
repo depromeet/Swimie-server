f639df9a03b3b84eb685a8458b2df025904c1f5a
penrose15 <seongju99@naver.com>
2024-08-16 18:48:00 +0900

    fix: FollowingLogErrorType 컴파일 에러 수정 (#219)

--- module-independent/src/main/java/com/depromeet/type/followingLog/FollowingLogErrorType.java
+++ module-independent/src/main/java/com/depromeet/type/followingLog/FollowingLogErrorType.java
@@ -1,3 +1,26 @@
-package com.depromeet.type.news;
+package com.depromeet.type.followingLog;
 
-public enum NewsErrorType {}
+import com.depromeet.type.ErrorType;
+
+public enum FollowingLogErrorType implements ErrorType {
+    ;
+
+    private final String code;
+
+    private final String message;
+
+    FollowingLogErrorType(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String getCode() {
+        return "";
+    }
+
+    @Override
+    public String getMessage() {
+        return "";
+    }
+}
