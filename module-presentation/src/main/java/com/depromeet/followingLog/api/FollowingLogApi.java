71baa049026cc9ab4b34e79985785345f1376e3a
penrose15 <seongju99@naver.com>
2024-08-17 23:02:03 +0900

    fix: swagger Tag 이름 변경(#219)

--- module-presentation/src/main/java/com/depromeet/followingLog/api/FollowingLogApi.java
+++ module-presentation/src/main/java/com/depromeet/followingLog/api/FollowingLogApi.java
@@ -7,7 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Tag(name = "팔로잉 소식 (FollowingLog)")
+@Tag(name = "팔로잉 소식 (Following News)")
 public interface FollowingLogApi {
     @Operation(summary = "팔로우 소식 조회")
     ApiResponse<FollowingLogSliceResponse> getFollowingLogs(
