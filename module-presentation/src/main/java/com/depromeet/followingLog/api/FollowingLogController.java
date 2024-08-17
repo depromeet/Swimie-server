06cf6cf5d06528e699a624ab2fca6a8df1fd728b
penrose15 <seongju99@naver.com>
2024-08-17 17:38:12 +0900

    feat: 팔로잉 소식 조회 API 이름 변경 (#219)

--- module-presentation/src/main/java/com/depromeet/followingLog/api/FollowingLogController.java
+++ module-presentation/src/main/java/com/depromeet/followingLog/api/FollowingLogController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/following-log")
+@RequestMapping("/news")
 public class FollowingLogController implements FollowingLogApi {
     private final FollowingLogFacade followingLogFacade;
 
