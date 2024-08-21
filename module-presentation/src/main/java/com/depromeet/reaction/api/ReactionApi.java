dabd8839cd24f66e0936ddf784da3c9b3a80ab08
its-sky <smc9919@naver.com>
2024-08-22 01:20:05 +0900

    feat: 응원 등록 가능 여부 검증 API Swagger 추가 (#248)

--- module-presentation/src/main/java/com/depromeet/reaction/api/ReactionApi.java
+++ module-presentation/src/main/java/com/depromeet/reaction/api/ReactionApi.java
@@ -5,6 +5,7 @@ import com.depromeet.member.annotation.LoginMember;
 import com.depromeet.reaction.dto.request.ReactionCreateRequest;
 import com.depromeet.reaction.dto.response.MemoryReactionResponse;
 import com.depromeet.reaction.dto.response.PagingReactionResponse;
+import com.depromeet.reaction.dto.response.ValidateReactionResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -20,18 +21,22 @@ public interface ReactionApi {
     ApiResponse<?> create(
             @LoginMember Long memberId, @Valid @RequestBody ReactionCreateRequest request);
 
+    @Operation(summary = "응원 가능 여부 확인")
+    ApiResponse<ValidateReactionResponse> validate(
+            @LoginMember Long memberId, @PathVariable("memoryId") Long memoryId);
+
     @Operation(summary = "수영 상세 기록 내 가로 스크롤 응원 조회")
-    ApiResponse<MemoryReactionResponse> read(@PathVariable(value = "memoryId") Long memoryId);
+    ApiResponse<MemoryReactionResponse> read(@PathVariable("memoryId") Long memoryId);
 
     @Operation(summary = "응원 전체 상세 조회")
     ApiResponse<PagingReactionResponse> read(
             @LoginMember Long memberId,
-            @PathVariable(value = "memoryId") Long memoryId,
+            @PathVariable("memoryId") Long memoryId,
             @Parameter(description = "다음 조회를 위한 커서 ID", example = "21")
                     @RequestParam(value = "cursorId", required = false)
                     Long cursorId);
 
     @Operation(summary = "응원 삭제")
     ResponseEntity<Void> delete(
-            @LoginMember Long memberId, @PathVariable(value = "reactionId") Long reactionId);
+            @LoginMember Long memberId, @PathVariable("reactionId") Long reactionId);
 }
