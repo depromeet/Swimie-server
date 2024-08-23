654d5f4dc569dce27fa7672250a8c3033e1d1fa4
penrose15 <seongju99@naver.com>
2024-08-23 23:22:37 +0900

    fix: http method가 delete인 경우 응답값 204 NoContent -> 200 으로 변경 (#254)

--- module-presentation/src/main/java/com/depromeet/reaction/api/ReactionController.java
+++ module-presentation/src/main/java/com/depromeet/reaction/api/ReactionController.java
@@ -11,7 +11,6 @@ import com.depromeet.reaction.facade.ReactionFacade;
 import com.depromeet.type.reaction.ReactionSuccessType;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -63,9 +62,9 @@ public class ReactionController implements ReactionApi {
 
     @Logging(item = "Reaction", action = "DELETE")
     @DeleteMapping("/memory/reaction/{reactionId}")
-    public ResponseEntity<Void> delete(
+    public ApiResponse<?> delete(
             @LoginMember Long memberId, @PathVariable("reactionId") Long reactionId) {
         reactionFacade.deleteById(memberId, reactionId);
-        return ResponseEntity.noContent().build();
+        return ApiResponse.success(ReactionSuccessType.DELETE_REACTIONS_SUCCESS);
     }
 }
