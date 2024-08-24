1efadb808ef676573ff3a8d16ead6711280b8dac
its-sky <smc9919@naver.com>
2024-08-24 20:14:00 +0900

    chore: 컨벤션 수정 (#250)

--- module-independent/src/main/java/com/depromeet/type/notification/NotificationErrorType.java
+++ module-independent/src/main/java/com/depromeet/type/notification/NotificationErrorType.java
@@ -4,8 +4,7 @@ import com.depromeet.type.ErrorType;
 
 public enum NotificationErrorType implements ErrorType {
     INVALID_NOTIFICATION_TYPE("NOTIFICATION_1", "알림 타입이 잘못되었습니다"),
-    NOT_FOUND("NOTIFICATION_2", "알림을 찾을 수 없습니다"),
-    ;
+    NOT_FOUND("NOTIFICATION_2", "알림을 찾을 수 없습니다");
 
     private final String code;
     private final String message;
