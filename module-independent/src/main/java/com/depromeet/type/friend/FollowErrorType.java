59c425b45512a5b61f71f5b04122471dff2ff651
its-sky <smc9919@naver.com>
2024-08-24 10:59:48 +0900

    feat: 팔로우 에러 타입 추가 (#250)

--- module-independent/src/main/java/com/depromeet/type/friend/FollowErrorType.java
+++ module-independent/src/main/java/com/depromeet/type/friend/FollowErrorType.java
@@ -4,7 +4,8 @@ import com.depromeet.type.ErrorType;
 
 public enum FollowErrorType implements ErrorType {
     NOT_FOUND("FOLLOW_1", "팔로잉 유저를 찾을 수 없습니다"),
-    SELF_FOLLOWING_NOT_ALLOWED("FOLLOW_2", "자기 자신을 팔로잉 할 수 없습니다");
+    SELF_FOLLOWING_NOT_ALLOWED("FOLLOW_2", "자기 자신을 팔로잉 할 수 없습니다"),
+    INVALID_FOLLOW_TYPE("FOLLOW_3", "올바르지 않은 팔로우 타입입니다");
 
     private final String code;
 
