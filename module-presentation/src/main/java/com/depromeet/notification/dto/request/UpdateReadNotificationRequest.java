1c3bdee7ebcd25daf55a06293f318dacc2143f64
its-sky <smc9919@naver.com>
2024-08-24 13:12:06 +0900

    refactor: 알림 읽음 처리 Request Body Validation 추가 (#250)

--- module-presentation/src/main/java/com/depromeet/notification/dto/request/UpdateReadNotificationRequest.java
+++ module-presentation/src/main/java/com/depromeet/notification/dto/request/UpdateReadNotificationRequest.java
@@ -1,3 +1,7 @@
 package com.depromeet.notification.dto.request;
 
-public record UpdateReadNotificationRequest(Long notificationId, String type) {}
+import jakarta.validation.constraints.NotNull;
+
+public record UpdateReadNotificationRequest(
+        @NotNull(message = "알림 ID를 입력하세요") Long notificationId,
+        @NotNull(message = "알림 타입을 입력하세요") String type) {}
