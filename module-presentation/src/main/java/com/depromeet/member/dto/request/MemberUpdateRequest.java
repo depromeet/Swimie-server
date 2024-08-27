4107dfea4d894b1bd6165455e415ef67e0721675
ywonchae1 <ywonchae62@gmail.com>
2024-08-27 21:43:57 +0900

    fix: 입력 값 검증 방식 변경 (#282)

--- module-presentation/src/main/java/com/depromeet/member/dto/request/MemberUpdateRequest.java
+++ module-presentation/src/main/java/com/depromeet/member/dto/request/MemberUpdateRequest.java
@@ -1,11 +1,9 @@
 package com.depromeet.member.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotBlank;
 
 public record MemberUpdateRequest(
-        @NotBlank(message = "멤버의 이름은 비어 있을 수 없습니다")
-                @Schema(description = "닉네임", requiredMode = Schema.RequiredMode.REQUIRED)
+        @Schema(description = "닉네임", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
                 String nickname,
         @Schema(description = "한 줄 소개", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
                 String introduction) {
