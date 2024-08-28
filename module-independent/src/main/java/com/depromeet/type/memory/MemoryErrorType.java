7afcecaf074f60057d48dec133a2cd8a7e70577b
penrose15 <seongju99@naver.com>
2024-08-28 17:25:30 +0900

    fix: meter 입력 시 0이상인지 유효성 검사 및 시작 시간이 종료 시간 보다 앞인지 체크 (#303)

--- module-independent/src/main/java/com/depromeet/type/memory/MemoryErrorType.java
+++ module-independent/src/main/java/com/depromeet/type/memory/MemoryErrorType.java
@@ -9,7 +9,8 @@ public enum MemoryErrorType implements ErrorType {
     FORBIDDEN("MEMORY_4", "메모리에 접근 권한이 없습니다"),
     ALREADY_CREATED("MEMORY_5", "해당 날짜에 이미 기록이 존재합니다"),
     NOT_FOUND_PREV("MEMORY_6", "이전 기록이 존재하지 않습니다"),
-    NOT_FOUND_NEXT("MEMORY_7", "다음 기록이 존재하지 않습니다");
+    NOT_FOUND_NEXT("MEMORY_7", "다음 기록이 존재하지 않습니다"),
+    TIME_NOT_VALID("MEMORY_8", "시작 시간은 종료 시간을 역전할 수 없습니다");
 
     private final String code;
     private final String message;
