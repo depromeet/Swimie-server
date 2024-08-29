30cb71c49af791bc1fcb70ad333121772aae1f66
ywonchae1 <ywonchae62@gmail.com>
2024-08-29 21:02:08 +0900

    feat: 애플 로그인 요청 객체 수정 (#323)

--- module-presentation/src/main/java/com/depromeet/auth/dto/request/AppleLoginRequest.java
+++ module-presentation/src/main/java/com/depromeet/auth/dto/request/AppleLoginRequest.java
@@ -1,5 +1,14 @@
 package com.depromeet.auth.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
-public record AppleLoginRequest(@NotNull(message = "인가 코드는 null일 수 없습니다") String code) {}
+public record AppleLoginRequest(
+        @Schema(description = "회원 정보", requiredMode = Schema.RequiredMode.NOT_REQUIRED)
+                AppleAccountInfo user,
+        @NotNull(message = "인가 코드는 null일 수 없습니다")
+                @Schema(description = "인가 코드", requiredMode = Schema.RequiredMode.REQUIRED)
+                String code,
+        @NotNull(message = "ID 토큰은 null일 수 없습니다")
+                @Schema(description = "ID 토큰", requiredMode = Schema.RequiredMode.REQUIRED)
+                String idToken) {}
