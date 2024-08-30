43a2af21ec38edd3f1b84e59aee1be36711b64e2
its-sky <smc9919@naver.com>
2024-08-30 16:31:00 +0900

    refactor: 디바운싱 로직 변경 (#335)

--- module-presentation/src/main/java/com/depromeet/common/DuplicateRequestAspect.java
+++ module-presentation/src/main/java/com/depromeet/common/DuplicateRequestAspect.java
@@ -3,26 +3,31 @@ package com.depromeet.common;
 import static java.util.concurrent.TimeUnit.*;
 
 import com.depromeet.exception.BaseException;
+import com.depromeet.friend.dto.request.FollowRequest;
 import com.depromeet.type.common.CommonErrorType;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
+import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
+@Slf4j
 @Aspect
 @Component
 public class DuplicateRequestAspect {
-    private Set<String> requestSet = Collections.synchronizedSet(new HashSet<>());
+    private Set<String> requestSet = new ConcurrentSkipListSet<>();
     private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
     @Pointcut("within(*..*Controller)")
@@ -39,19 +44,58 @@ public class DuplicateRequestAspect {
             return joinPoint.proceed();
         }
 
-        String requestId = joinPoint.getSignature().toLongString();
+        String requestURI = request.getRequestURI();
+        String userId = getUserIdFromSecurityContext();
+
+        String requestId = userId + joinPoint.getSignature().toLongString();
+        requestId = handleWhitelist(joinPoint, requestURI, requestId);
+
         if (requestSet.contains(requestId)) {
             handleDuplicateRequest();
         }
         requestSet.add(requestId);
+
         try {
             return joinPoint.proceed();
         } finally {
-            scheduler.schedule(() -> requestSet.remove(requestId), 1, SECONDS);
+            final String finalRequestId = requestId;
+            scheduler.schedule(() -> requestSet.remove(finalRequestId), 1, SECONDS);
         }
+    }
+
+    private String handleWhitelist(
+            ProceedingJoinPoint joinPoint, String requestURI, String requestId) {
+        if (requestURI.equals("/friend")) {
+            Object[] args = joinPoint.getArgs();
+            Object requestBody = null;
+
+            for (Object arg : args) {
+                if (arg instanceof FollowRequest) {
+                    requestBody = arg;
+                    break;
+                }
+            }
+
+            if (requestBody != null) {
+                FollowRequest requests = (FollowRequest) requestBody;
+                requestId += requests.followingId().toString();
+            }
+        }
+        return requestId;
     }
 
     private void handleDuplicateRequest() {
         throw new BaseException(CommonErrorType.TOO_MANY_REQUESTS, HttpStatus.TOO_MANY_REQUESTS);
+    }
+
+    private String getUserIdFromSecurityContext() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated()) {
+            Object principal = authentication.getPrincipal();
+            if (principal instanceof UserDetails) {
+                return ((UserDetails) principal).getUsername();
+            }
+        }
+        return null;
     }
 }
