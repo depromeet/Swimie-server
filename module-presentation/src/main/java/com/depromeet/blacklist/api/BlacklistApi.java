c547880b6977155bbb66221ea7284816210597cc
its-sky <smc9919@naver.com>
2024-09-07 01:53:50 +0900

    feat: 차단한 사용자 조회 API Swagger 추가 (#385)

--- module-presentation/src/main/java/com/depromeet/blacklist/api/BlacklistApi.java
+++ module-presentation/src/main/java/com/depromeet/blacklist/api/BlacklistApi.java
@@ -1,6 +1,7 @@
 package com.depromeet.blacklist.api;
 
 import com.depromeet.blacklist.dto.request.BlackMemberRequest;
+import com.depromeet.blacklist.dto.response.BlackMemberResponse;
 import com.depromeet.dto.response.ApiResponse;
 import com.depromeet.member.annotation.LoginMember;
 import io.swagger.v3.oas.annotations.Operation;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "차단(Blacklist)")
 public interface BlacklistApi {
@@ -18,4 +20,9 @@ public interface BlacklistApi {
     @Operation(summary = "사용자 차단 해제")
     ApiResponse<?> unblackMember(
             @LoginMember Long memberId, @PathVariable("blackMemberId") Long blackMemberId);
+
+    @Operation(summary = "사용자 차단 목록 조회")
+    ApiResponse<BlackMemberResponse> getBlackMembers(
+            @LoginMember Long memberId,
+            @RequestParam(value = "cursorId", required = false) Long cursorId);
 }
