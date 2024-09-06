df8e406ec9d9a7bc202bffa5ca13a92fe36c3d71
its-sky <smc9919@naver.com>
2024-09-07 01:53:43 +0900

    feat: 차단한 사용자 조회 API 구현 (#385)

--- module-domain/src/main/java/com/depromeet/blacklist/port/in/usecase/BlacklistQueryUseCase.java
+++ module-domain/src/main/java/com/depromeet/blacklist/port/in/usecase/BlacklistQueryUseCase.java
@@ -1,5 +1,9 @@
 package com.depromeet.blacklist.port.in.usecase;
 
+import com.depromeet.blacklist.domain.vo.BlacklistPage;
+
 public interface BlacklistQueryUseCase {
     boolean checkBlackMember(Long memberId, Long blackMemberId);
+
+    BlacklistPage getBlackMembers(Long memberId, Long cursorId);
 }
--- module-domain/src/main/java/com/depromeet/blacklist/service/BlacklistService.java
+++ module-domain/src/main/java/com/depromeet/blacklist/service/BlacklistService.java
@@ -1,10 +1,12 @@
 package com.depromeet.blacklist.service;
 
 import com.depromeet.blacklist.domain.Blacklist;
+import com.depromeet.blacklist.domain.vo.BlacklistPage;
 import com.depromeet.blacklist.port.in.usecase.BlacklistCommandUseCase;
 import com.depromeet.blacklist.port.in.usecase.BlacklistQueryUseCase;
 import com.depromeet.blacklist.port.out.persistence.BlacklistPersistencePort;
 import com.depromeet.member.domain.Member;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,5 +36,18 @@ public class BlacklistService implements BlacklistQueryUseCase, BlacklistCommand
     @Override
     public boolean checkBlackMember(Long memberId, Long blackMemberId) {
         return blacklistPersistencePort.existsByMemberIdAndBlackMemberId(memberId, blackMemberId);
+    }
+
+    @Override
+    public BlacklistPage getBlackMembers(Long memberId, Long cursorId) {
+        List<Member> blackMembers = blacklistPersistencePort.findBlackMembers(memberId, cursorId);
+        boolean hasNext = false;
+        Long nextCursorId = null;
+        if (blackMembers != null && blackMembers.size() > 10) {
+            hasNext = true;
+            Member member = blackMembers.removeLast();
+            nextCursorId = member.getId();
+        }
+        return BlacklistPage.of(blackMembers, hasNext, nextCursorId);
     }
 }
