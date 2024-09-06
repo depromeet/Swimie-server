7c93cdf01587437bb55e1c3e5f35318906887b46
its-sky <smc9919@naver.com>
2024-09-06 23:36:46 +0900

    feat: Blacklist Repository 및 Port 구현 (#377)

---
+++ module-domain/src/main/java/com/depromeet/blacklist/port/out/persistence/BlacklistPersistencePort.java
@@ -0,0 +1,9 @@
+package com.depromeet.blacklist.port.out.persistence;
+
+import com.depromeet.blacklist.domain.Blacklist;
+
+public interface BlacklistPersistencePort {
+    Blacklist save(Blacklist blacklist);
+
+    boolean existsByMemberIdAndBlackMemberId(Long memberId, Long blackMemberId);
+}
