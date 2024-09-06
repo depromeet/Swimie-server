4e4fb444e0bc67bcb2e2c9f48b518ca2856a0c4e
its-sky <smc9919@naver.com>
2024-09-06 23:47:23 +0900

    chore: 코드리뷰 반영 (#377)

--- module-presentation/src/main/java/com/depromeet/blacklist/dto/request/BlackMemberRequest.java
+++ module-presentation/src/main/java/com/depromeet/blacklist/dto/request/BlackMemberRequest.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
 public record BlackMemberRequest(
-        @Schema(
+        @NotNull
+                @Positive
+                @Schema(
                         description = "차단 대상 사용자 ID",
                         example = "1",
                         requiredMode = Schema.RequiredMode.REQUIRED)
-                @NotNull
-                @Positive
                 Long blackMemberId) {}
