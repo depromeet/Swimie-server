7cbe4acc9fc9680f9e25af56e9d5462a2d97d048
ywonchae1 <ywonchae62@gmail.com>
2024-09-11 03:11:22 +0900

    chore: spotless 적용 (#413)

--- module-presentation/src/main/java/com/depromeet/auth/facade/AuthFacade.java
+++ module-presentation/src/main/java/com/depromeet/auth/facade/AuthFacade.java
@@ -85,7 +85,8 @@ public class AuthFacade {
     public JwtTokenResponse loginByApple(AppleLoginRequest request) {
         final AccountProfileResponse profile =
                 socialUseCase.getAppleAccountToken(
-                        AppleMapper.toCommand(request), "https://patient-ocelot-accurate.ngrok-free.app");
+                        AppleMapper.toCommand(request),
+                        "https://patient-ocelot-accurate.ngrok-free.app");
         if (profile == null) {
             throw new NotFoundException(AuthErrorType.NOT_FOUND);
         }
