85287442619a79bee9ea9d00355579387789b91f
penrose15 <seongju99@naver.com>
2024-09-11 21:25:50 +0900

    refactor: MDC request_id를 UUID -> IP 로 변경 (#353)

--- module-presentation/src/main/java/com/depromeet/config/log/mdc/MDCFilter.java
+++ module-presentation/src/main/java/com/depromeet/config/log/mdc/MDCFilter.java
@@ -2,7 +2,6 @@ package com.depromeet.config.log.mdc;
 
 import jakarta.servlet.*;
 import java.io.IOException;
-import java.util.UUID;
 import org.slf4j.MDC;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
@@ -15,9 +14,9 @@ public class MDCFilter implements Filter {
     public void doFilter(
             ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
             throws IOException, ServletException {
-        final UUID uuid = UUID.randomUUID();
-        String requestId = uuid.toString().replaceAll("-", "");
-        MDC.put("request_id", requestId);
+
+        String clientIp = servletRequest.getRemoteAddr();
+        MDC.put("request_id", clientIp);
         filterChain.doFilter(servletRequest, servletResponse);
         MDC.clear();
     }
