9afed2c005453140a336cdde4d1ca99e9b49fda1
its-sky <smc9919@naver.com>
2024-10-25 00:33:44 +0900

    refactor: 인삿말 HTTP 캐시 적용 (#450)

--- module-presentation/src/main/java/com/depromeet/greeting/api/GreetingController.java
+++ module-presentation/src/main/java/com/depromeet/greeting/api/GreetingController.java
@@ -4,7 +4,10 @@ import com.depromeet.dto.response.ApiResponse;
 import com.depromeet.greeting.dto.response.GreetingResponse;
 import com.depromeet.greeting.facade.GreetingFacade;
 import com.depromeet.type.ai.AISuccessType;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,8 +17,11 @@ public class GreetingController implements GreetingApi {
     private final GreetingFacade greetingFacade;
 
     @GetMapping
-    public ApiResponse<GreetingResponse> getGreeting() {
+    public ResponseEntity<?> getGreeting() {
         GreetingResponse response = greetingFacade.getGreeting();
-        return ApiResponse.success(AISuccessType.GET_RESPONSE_SUCCESS, response);
+        CacheControl cacheControl = CacheControl.maxAge(Duration.ofHours(1)).cachePrivate();
+        ApiResponse<GreetingResponse> apiResponse =
+                ApiResponse.success(AISuccessType.GET_RESPONSE_SUCCESS, response);
+        return ResponseEntity.ok().cacheControl(cacheControl).body(apiResponse);
     }
 }
