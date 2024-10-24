63e455dd9c202c6a0b1f5af240492238ba407053
its-sky <smc9919@naver.com>
2024-10-25 00:22:54 +0900

    remove: 불필요한 코드 삭제 (#450)

--- module-independent/src/main/java/com/depromeet/dto/ai/ChatCompletionsRequest.java
+++ module-independent/src/main/java/com/depromeet/dto/ai/ChatCompletionsRequest.java
@@ -2,17 +2,6 @@ package com.depromeet.dto.ai;
 
 import java.util.List;
 
-// public record ChatCompletionsRequest(List<ChatCompletionsMessage> messages) {
-//     public static ChatCompletionsRequest of(String systemContent, String userContent) {
-//         ChatCompletionsMessage systemMessage = ChatCompletionsMessage.of("system",
-// systemContent);
-//         ChatCompletionsMessage userMessage = ChatCompletionsMessage.of("user", userContent);
-//         List<ChatCompletionsMessage> messages = List.of(systemMessage, userMessage);
-//
-//         return new ChatCompletionsRequest(messages);
-//     }
-// }
-
 public record ChatCompletionsRequest(
         List<ChatCompletionsMessage> messages,
         double topP,
